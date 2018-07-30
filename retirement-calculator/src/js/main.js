acbdc84bf246a264b8ce5f7767d4bd19d6ae1f50
timwheelercom <tim@timwheeler.com>
2018-07-30 12:29:04 -0400

    added progress bar feature

--- retirement-calculator/src/js/main.js
+++ retirement-calculator/src/js/main.js
@@ -70,100 +70,103 @@ const client = {
 
 const handlers = {
 
-  // setupEventListeners: () => {
-  //   const form = document.getElementById('calculator');
-  //
-  //   form.addEventListener('click', (e) => {
-  //     e.preventDefault();
-  //
-  //     if(e.target.id === 'nextBtn-1') {
-  //       calculator.pages.pg1.classList.add('displayNone');
-  //       calculator.pages.pg2.classList.remove('displayNone');
-  //       handlers.updateClientAge();
-  //       handlers.updateClientAnnualIncome();
-  //     }
-  //
-  //     if(e.target.id === 'nextBtn-2') {
-  //       calculator.pages.pg2.classList.add('displayNone');
-  //       calculator.pages.pg3.classList.remove('displayNone');
-  //       handlers.updateClientCurrentRetirementSavings();
-  //       handlers.updateClientPercentageAnnualRaises();
-  //     }
-  //
-  //     if(e.target.id === 'nextBtn-3') {
-  //       calculator.pages.pg3.classList.add('displayNone');
-  //       calculator.pages.pg4.classList.remove('displayNone');
-  //       handlers.updateClientExpectedRetirementAge();
-  //       handlers.updateClientExpectedRetirementLength();
-  //     }
-  //
-  //     if(e.target.id === 'nextBtn-4') {
-  //       calculator.pages.pg4.classList.add('displayNone');
-  //       calculator.pages.pg5.classList.remove('displayNone');
-  //       handlers.updateClientPercentPreRetirementIncome();
-  //       handlers.updateClientTaxReturnBeforeRetirement();
-  //       handlers.updateClientTaxReturnAfterRetirement();
-  //     }
-  //
-  //     if(e.target.id === 'submitBtn') {
-  //       // calculator.pages.pg4.classList.add('displayNone');
-  //       handlers.updateClientFirstName();
-  //       handlers.updateClientLastName();
-  //       handlers.updateClientEmail();
-  //       handlers.updateClientPhone();
-  //       calculator.clientInfo.section.classList.remove('displayNone');
-  //       console.log(client);
-  //       console.log(JSON.stringify(client));
-  //
-  //       $.ajax({
-  //         type: "POST",
-  //         url: "https://mandrillapp.com/api/1.0/messages/send.json",
-  //         data: {
-  //           'key': 'xWpoj3QqZHv7pDNIz1VJEg',
-  //           'message': {
-  //             'from_email': 'tim@devplum.com',
-  //             'to': [
-  //               {
-  //                 'email': 'tim@devplum.com',
-  //                 'name': 'Plum Direct Marketing',
-  //                 'type': 'to'
-  //               },
-  //               {
-  //                 'email': client.email,
-  //                 'name': client.firstName + '' + client.lastName,
-  //                 'type': 'to'
-  //               }
-  //             ],
-  //             'autotext': 'true',
-  //             'subject': 'YOUR SUBJECT HERE!',
-  //             'html': JSON.stringify(client)
-  //           }
-  //         }
-  //       }).done(function(response) {
-  //         console.log(response); // if you're into that sorta thing
-  //       });
-  //
-  //
-  //     }
-  //
-  //   });
-  // },
+  setupEventListeners: () => {
+    const form = document.getElementById('calculator');
 
-  setupEventListeners: () =>  {
-    let pages = document.querySelectorAll('#calculator>div');
-    for (let i = 0; i < pages.length; i++) {
-      if (pages[i].style.display !== 'none') {
-        pages[i].style.display = 'none';
-        if (i === pages.length - 1) {
-          pages[0].style.display = 'block';
-          document.getElementById('clientInfo').style.display = 'block';
-        } else {
-          pages[i + 1].style.display = 'block';
-        }
-        break;
+    form.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      if(e.target.id === 'nextBtn-1') {
+        calculator.pages.pg1.style.display = 'none';
+        calculator.pages.pg2.style.display = 'block';
+        handlers.updateClientAge();
+        handlers.updateClientAnnualIncome();
+        progress.increment(20);
       }
-    }
+
+      if(e.target.id === 'nextBtn-2') {
+        calculator.pages.pg2.style.display = 'none';
+        calculator.pages.pg3.style.display = 'block';
+        handlers.updateClientCurrentRetirementSavings();
+        handlers.updateClientPercentageAnnualRaises();
+        progress.increment(20);
+      }
+
+      if(e.target.id === 'nextBtn-3') {
+        calculator.pages.pg3.style.display = 'none';
+        calculator.pages.pg4.style.display = 'block';
+        handlers.updateClientExpectedRetirementAge();
+        handlers.updateClientExpectedRetirementLength();
+        progress.increment(20);
+      }
+
+      if(e.target.id === 'nextBtn-4') {
+        calculator.pages.pg4.style.display = 'none';
+        calculator.pages.pg5.style.display = 'block';
+        handlers.updateClientPercentPreRetirementIncome();
+        handlers.updateClientTaxReturnBeforeRetirement();
+        handlers.updateClientTaxReturnAfterRetirement();
+        progress.increment(20);
+      }
+
+      if(e.target.id === 'submitBtn') {
+        calculator.pages.pg4.style.display = 'none';
+        handlers.updateClientFirstName();
+        handlers.updateClientLastName();
+        handlers.updateClientEmail();
+        handlers.updateClientPhone();
+        calculator.clientInfo.section.style.display = 'block';
+        console.log(client);
+        console.log(JSON.stringify(client));
+        progress.complete();
+
+        $.ajax({
+          type: "POST",
+          url: "https://mandrillapp.com/api/1.0/messages/send.json",
+          data: {
+            'key': 'xWpoj3QqZHv7pDNIz1VJEg',
+            'message': {
+              'from_email': 'tim@devplum.com',
+              'to': [
+                {
+                  'email': 'tim@devplum.com',
+                  'name': 'Plum Direct Marketing',
+                  'type': 'to'
+                },
+                {
+                  'email': client.email,
+                  'name': client.firstName + '' + client.lastName,
+                  'type': 'to'
+                }
+              ],
+              'autotext': 'true',
+              'subject': 'YOUR SUBJECT HERE!',
+              'html': JSON.stringify(client)
+            }
+          }
+        }).done(function(response) {
+          console.log(response); // if you're into that sorta thing
+        });
+      }
+
+    });
   },
+
+  // setupEventListeners: () =>  {
+  //   let pages = document.querySelectorAll('#calculator>div');
+  //   for (let i = 0; i < pages.length; i++) {
+  //     if (pages[i].style.display !== 'none') {
+  //       pages[i].style.display = 'none';
+  //       if (i === pages.length - 1) {
+  //         pages[0].style.display = 'block';
+  //         document.getElementById('clientInfo').style.display = 'block';
+  //       } else {
+  //           pages[i + 1].style.display = 'block';
+  //       }
+  //       break;
+  //     }
+  //   }
+  // },
 
   updateClientAge: () => {
 
@@ -242,3 +245,55 @@ const handlers = {
 };
 
 // handlers.setupEventListeners(); // Setup event listeners for the form
+
+
+// Progress bar
+var progress = (function ($) {
+  var progress = $('.progress'),
+    progress_bar = $('.progress-bar'),
+    total_width = progress.width();
+  function calculatePercentage(increment_by,is_increment) {
+    var progress_percentage;
+    if (is_increment == true) {
+      progress_percentage = Math.round((progress_bar.width() / total_width) * 100 + increment_by) ;
+      progress_percentage = (progress_percentage > 100) ? 100 : progress_percentage;
+    } else {
+      progress_percentage = Math.round((progress_bar.width() / total_width) * 100 - increment_by) ;
+      progress_percentage = (progress_percentage < 0) ? 0 : progress_percentage;
+    }
+    return progress_percentage;
+  }
+  return{
+    increment: function (increment_by) {
+      var progress_percentage = calculatePercentage(increment_by, true);
+      progress_bar.css('width',progress_percentage + '%').attr('aria-valuenow', progress_percentage + ' %');
+    },
+    decrement: function (decrement_by) {
+      var progress_percentage = calculatePercentage(decrement_by, false);
+      progress_bar.css('width',progress_percentage+'%').attr('aria-valuenow', progress_percentage + ' %');
+    },
+    reset: function () {
+      progress_bar.css('width',0 + '%').attr('aria-valuenow', 0 + ' %');
+    },
+    complete: function () {
+      progress_bar.css('width',100 + '%').attr('aria-valuenow', 100 + ' %');
+    }
+  };
+})( jQuery);
+
+
+
+
+// Examine these results carefully. As you can see, there is a lot of information to consider and the numbers may look large.
+
+// The annual income that you will need in your first year of retirement:	$0
+
+// When you begin retirement, you will need to have the following amount saved:	$0
+
+// Assuming no additional contributions, your current savings will grow to:	$1
+
+// Without additional savings, you will have a shortfall of:	$0
+
+// You will need to save the following percentage of your salary each year:	0%
+
+// In the first year, this works out to:	$0
